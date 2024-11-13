bfc53e4059fa65c13a110ffd8c19d9125197deee
EmilianoIbarraMX <augusstop@gmail.com>
2024-11-13 10:26:33 -0600

    Update Service-worker.js

--- Service-worker.js
+++ Service-worker.js
@@ -1,15 +1,15 @@
 const CACHE_NAME = 'mi-pwa-cache-v1';
 const urlsToCache = [
 './',
-'•/index.html',
-'•/app.js',
+'./index.html',
+'./app.js',
 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
 1;
 // Evento de instalación
 self.addEventListener ('install', event => {
 event.waitUntil(
 caches. open (CACHE_NAME)
-• then (cache → {
+.then (cache → {
 console. log ('Abriendo caché'); return cache.addAll (urlsToCache);
 })
 );
@@ -20,7 +20,7 @@ console. log ('Abriendo caché'); return cache.addAll (urlsToCache);
 self.addEventListener('fetch', event => {
 event. respondWith(
 caches.match (event.request)
-• then (response => { return response || fetch(event. request);
+.then (response => { return response || fetch(event. request);
 })
 ) ;
 }) :
